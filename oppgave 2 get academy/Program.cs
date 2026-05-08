6a0776b355599f2ceb04d28f68ba3c1342c140a0
omar84-html <abdoboy666@hotmail.com>
2026-05-09 01:31:07 +0200

    Add project files.

---
+++ oppgave 2 get academy/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hello, World!");
+int alder = 20;
+long befolkning = 8000000000;
+float temperatur = 36.6F;
+decimal pris = 99.99M;
+double vekt = 75.5;
+string navn = "Omar";
+char bokstav = 'A';
+bool erStudent = true;
